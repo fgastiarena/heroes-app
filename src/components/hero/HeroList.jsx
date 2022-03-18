413e31840c3ec21e921c01aaeac4e9a93d2de3cb
Fernanda <mf.gastiarena@gmail.com>
2022-03-18 10:26:01 -0300

    Primer commit

---
+++ src/components/hero/HeroList.jsx
@@ -0,0 +1,21 @@
+import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
+
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroesByPublisher(publisher);
+
+    return (
+        <div className='row rows-col-1 row-cols-md-3 g-3'>
+            {
+                heroes.map(hero => (
+                    <HeroCard 
+                        key={hero.id}
+                            {...hero}
+                    />
+                )) //
+            }
+        </div>
+    )
+}
