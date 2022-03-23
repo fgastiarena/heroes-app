b0fd6dea7b96edce3af31316d352f78efd92b25a
Fernanda <mf.gastiarena@gmail.com>
2022-03-23 11:26:54 -0300

    Proteccion de rutas

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
+
 
 export const Navbar = () => {
 
+    const {user, dispatch} = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleLogout = () => {
+
+        dispatch({type: types.logout});
+      
         navigate('/login', {
             replace: true
         });    
@@ -53,7 +60,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className='nav-item nav-link text-info'>
-                        Fernanda
+                        {user.name}
                     </span>
 
                     <button 
